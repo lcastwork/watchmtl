0610e0a9a26e24251a64a3d1a2c4012de7137e42
luiscast <luiscast.work@gmail.com>
2019-03-21 17:14:39 -0400

    Agregado sass y pre-compiladores --estado limpio

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 const gulp = require("gulp");
 const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+
+gulp.task("sass", function() {
+  return gulp
+    .src(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"])
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
 
 // Vigilar Sass y Servidor
-gulp.task("serve", function() {
+gulp.task("serve", ["sass"], function() {
   browserSync.init({
     server: "./src"
   });
 
   gulp.watch("src/*.html").on("change", browserSync.reload);
-  gulp.watch("src/css/*.css").on("change", browserSync.reload);
+  gulp.watch(["src/scss/*.scss"], ["sass"]).on("change", browserSync.reload);
 });
 
 gulp.task("default", ["serve"]);
